17f6b4c81a4bc303221fb49a1a929a4799c71df1
omarakamal <omaraakamal@gmail.com>
2025-11-24 18:51:28 +0300

    update

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ const todoInputElement = document.querySelector('#todo-input')
 const submitButtonElement = document.querySelector('#submit-button')
 
 const todoListElement = document.querySelector('#todo-list')
+
+console.log(todoInputElement)
+console.log(submitButtonElement)
+console.log(todoListElement)
+
+
+todoInputElement.value = 'Abulrahman'
+
+
+function addToDoItem(){
+    console.log('todo item add')
+}
+
+
+// Events
+// 2 arguments of the addEventListener
+// 1. what type of event
+// 2. function
+
+submitButtonElement.addEventListener('click')
